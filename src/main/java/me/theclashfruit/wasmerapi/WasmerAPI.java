35b3fa9dbba44bf635ef3023ac394c41ddd3eacf
TheClashFruit <clash.fruit123@gmail.com>
2024-04-05 23:34:47 +0200

    feat: start of registry api

--- src/main/java/me/theclashfruit/wasmerapi/WasmerAPI.java
+++ src/main/java/me/theclashfruit/wasmerapi/WasmerAPI.java
@@ -3,8 +3,15 @@ package me.theclashfruit.wasmerapi;
 import me.theclashfruit.wasmer.api.registry.MethodRegistry;
 import me.theclashfruit.wasmerapi.methods.registry.Register;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class WasmerAPI implements ModInitializer {
+
+    public static final String MOD_ID = "wasmerapi";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
         MethodRegistry.register(new Register());
--- src/main/java/me/theclashfruit/wasmerapi/methods/registry/Register.java
+++ src/main/java/me/theclashfruit/wasmerapi/methods/registry/Register.java
@@ -4,14 +4,34 @@ import me.theclashfruit.wasmer.api.WasmMethod;
 
 import com.dylibso.chicory.runtime.Instance;
 import com.dylibso.chicory.wasm.types.Value;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+import static me.theclashfruit.wasmerapi.WasmerAPI.LOGGER;
 
 public class Register extends WasmMethod {
     public Register() {
-        this.fieldName = "register";
+        this.fieldName = "registry_register";
     }
 
     @Override
     public Value[] execute(Instance inst, Value... args) {
+        var id = getString(inst, args[1].asInt(), args[2].asInt()).split(":");
+
+        switch (args[0].asInt()) {
+            case 0 -> Registry.register(Registries.BLOCK, new Identifier(id[0], id[1]), null);
+            case 1 -> Registry.register(Registries.ITEM, new Identifier(id[0], id[1]), getItem(inst, args));
+        }
+
         return null;
+    }
+
+    private Item getItem(Instance inst, Value... args) {
+        LOGGER.info(getString(inst, args[3].asInt(), args[4].asInt()));
+
+        return new Item(new FabricItemSettings());
     }
 }
